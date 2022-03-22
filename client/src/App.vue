d796c3c2eb23bfbdbffd6d8c86883b89d71b187b
Dioauliaharrisa <daharrisa@gmail.com>
2022-03-22 21:48:44 +0700

    separating pages:done for museum page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,78 +1,5 @@
 <template>
   <div id="app">
-    <a-scene>
-      <a-camera
-        position="0 0 0"
-        wasd-controls-enabled="true"
-        fov="60"
-        near="0.1"
-        far="100"
-      >
-        <a-cursor></a-cursor>
-      </a-camera>
-      <!-- x y z -->
-      <!-- x = plane -->
-      <!-- y = heigth -->
-      <!-- @click="callWikipedia(0)" -->
-      <a-box
-        @click="callWikipedia(0)"
-        :src="museumImage0"
-        position="-18.5 3 -8.3"
-        rotation="0 70 0"
-        width="10"
-        height="6"
-        depth="0.1"
-      ></a-box>
-      <a-box
-        @click="callWikipedia(1)"
-        :src="museumImage1"
-        position="-8.3 4 -19.8"
-        rotation="0 21 0"
-        width="10"
-        height="6"
-        depth="0.1"
-      ></a-box>
-      <a-box
-        @click="callWikipedia(2)"
-        :src="museumImage2"
-        position="6.5 4 -19.5"
-        rotation="0 -22 0"
-        width="5"
-        height="8"
-        depth="0.1"
-      ></a-box>
-      <a-assets>
-        <img id="texture" src="5459923.jpg" />
-      </a-assets>
-      <a-assets>
-        <img id="wallpapertexture" src="wallpaper.jpg" />
-      </a-assets>
-
-      <!-- FLOOR -->
-      <a-plane
-        position="0 -3 0"
-        rotation="90 0 0"
-        width="170"
-        height="250"
-        src="#texture"
-        side="double"
-        repeat="1 1"
-      ></a-plane>
-      <a-sky color="#222"></a-sky>
-
-      <a-cylinder
-        position="-1 2 -3"
-        rotation="0 90 0"
-        radius="20"
-        segments-radial="8"
-        height="15"
-        color="bisque"
-        side="double"
-        metalness="0.7"
-        src="#wallpapertexture"
-        repeat="20 5"
-      ></a-cylinder>
-    </a-scene>
     <router-view />
   </div>
 </template>
@@ -80,42 +7,7 @@
 <script>
 export default {
   name: "A-Frame",
-  methods: {
-    async callWikipedia(id) {
-      try {
-        const response = await this.$store.dispatch(`callWikipedia`, id);
-        window.open(response.data.data);
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
-  computed: {
-    museumImage0() {
-      if (this.$store.state.museumData) {
-        return this.$store.state.museumData?.artObjects[0].webImage.url;
-      } else {
-        return console.log(``);
-      }
-    },
-    museumImage1() {
-      if (this.$store.state.museumData) {
-        return this.$store.state.museumData?.artObjects[1].webImage.url;
-      } else {
-        return console.log(``);
-      }
-    },
-    museumImage2() {
-      if (this.$store.state.museumData) {
-        return this.$store.state.museumData?.artObjects[2].webImage.url;
-      } else {
-        return console.log(``);
-      }
-    },
-  },
-  async created() {
-    await this.$store.dispatch("callRijksmuseum");
-  },
+  
 };
 </script>
 
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,20 +1,21 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import VirtualMuseumPage from "../views/VirtualMuseumPage.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
-]
+  {
+    path: "/",
+    name: "VirtualMuseumPage",
+    component: VirtualMuseumPage,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
