244db98dbb4c6ecd7ff15bfb8053110fc4a6114f
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2022-03-23 06:18:15 +0700

    connect readNovel to server

--- src/store/index.js
+++ src/store/index.js
@@ -8,11 +8,15 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state: {
         allNovel: [],
+        novelToRead: [],
     },
     getters: {},
     mutations: {
         setAllNovels(state, novels) {
             state.allNovel = novels;
+        },
+        setNovelToRead(state, novel) {
+            state.novelToRead = novel;
         },
     },
     actions: {
@@ -28,6 +32,24 @@ export default new Vuex.Store({
                 swal('Error!', error.response.data.msg, 'error');
             }
         },
+        readNovel: async ({ commit }, { link, chapter }) => {
+            try {
+                console.log(`masukasdasdasd`, link, chapter);
+                let url = `/novel/title/${link.split('/')[4]}`;
+                if (!chapter) {
+                    url = `/novel/title/${link.split('/')[4]}/chapter-1`;
+                } else {
+                    url = `/novel/title/${
+                        link.split('/')[3]
+                    }/chapter-${chapter}`;
+                }
+                const { data } = await local.get(url);
+                commit('setNovelToRead', { data, url });
+            } catch (error) {
+                swal('Error!', error.response.data.msg, 'error');
+            }
+        },
     },
+
     modules: {},
 });
