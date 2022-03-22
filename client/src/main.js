a861bdaf8ca3bf1df222e944dcecc59d2e6ac0fe
ignatius andri pratama putra <ignaway@gmail.com>
2022-03-23 00:36:58 +0700

    galerry update lazy loading, mansory, animate scroll

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLazyload from 'vue-lazyload'
+import VueAnimate from 'vue-animate-scroll'
+ 
+Vue.use(VueAnimate)
 
 Vue.config.productionTip = false
 
+Vue.use(VueLazyload)
+// or with options
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'dist/error.png',
+  loading: 'dist/loading.gif',
+  attempt: 1,
+  listenEvents:['scroll','mousewheel'],
+})
+ 
 new Vue({
   router,
   store,
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col" v-for="data in photos" :key="data.id">
+          <img v-animate="'slide-up'" v-lazy="data.src.portrait" alt="" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  computed: {
+    photos() {
+      return this.$store.state.photos;
+    },
+  },
+  methods: {
+    fetchPhotos() {
+      this.$store.dispatch("fetchPhotos");
+    },
+  },
+  created() {
+    this.fetchPhotos();
+  },
+};
 </script>
+<style scoped>
+img {
+  width: 240px;
+  margin-bottom: calc(var(--bs-gutter-x) * 0.5);
+  margin-top: calc(var(--bs-gutter-x) * 0.5);
+}
+.animate {
+  transition-delay: 1.1s;
+  transition-duration: 1.25s;
+  transition-timing-function: ease-in
+}
+ 
+.slide-up {
+  transform: translateY(0);
+}
+ 
+.slide-up.animate-active {
+  transform: translateY(100px);
+}
+</style>
