f4369d0e3f4549d4666bcba090914bb48b0de768
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2022-03-23 05:43:59 +0700

    remove hello world and delete about routes

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,30 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        allNovel: [],
+    },
+    getters: {},
+    mutations: {
+        setAllNovels(state, novels) {
+            state.allNovel = novels;
+        },
+    },
+    actions: {
+        getAllNovels: async ({ commit }, params) => {
+            try {
+                let query = `/novel`;
+                if (params?.filter) query += `/${params.filter}`;
+                if (params?.search) query += `?order=${params.search}`;
+                const { data } = await local.get(query);
+                commit('setAllNovels', data);
+            } catch (error) {
+                // swal('Error!', error.response.data.msg, 'error');
+            }
+        },
+    },
+    modules: {},
+});
