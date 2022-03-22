e58d96cc893240371d1180ab47fb1b106cfad91b
ignatius andri pratama putra <ignaway@gmail.com>
2022-03-22 21:14:55 +0700

    update new dependensis

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <div id='map' style='width: 400px; height: 300px;'></div>
   </div>
 </template>
-<style scoped>
-body { margin: 0; padding: 0; }
-#map { position: absolute; top: 0; bottom: 0; width: 100%; }
-
-</style>
