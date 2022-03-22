f4f8302645333c7d5cad7cb1708d09f3c6bfaa8b
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2022-03-23 06:02:29 +0700

    add navbar and footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,107 @@
+<template>
+    <footer class="bg-gray-800">
+        <div class="container mx-auto px-6 pt-10 pb-10">
+            <div class="flex flex-wrap">
+                <div class="w-full lg:w-6/12 px-4">
+                    <h3 class="font-bold text-xl mb-2 text-white">About</h3>
+                    <p class="text-gray-300 text-sm">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Voluptatibus quia, nulla! Maiores et perferendis
+                        eligendi rerum eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
+                <div class="w-full lg:w-3/12 px-4">
+                    <h3 class="font-bold text-xl mb-2 text-white">Legal</h3>
+                    <ul class="list-reset">
+                        <li>
+                            <a
+                                href="#"
+                                class="text-gray-300 hover:text-white text-sm"
+                            >
+                                Lorem ipsum dolor sit amet
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                class="text-gray-300 hover:text-white text-sm"
+                            >
+                                Consectetur adipisicing elit
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                class="text-gray-300 hover:text-white text-sm"
+                            >
+                                Sed do eiusmod tempor incididunt
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                class="text-gray-300 hover:text-white text-sm"
+                            >
+                                Labore et dolore magna aliqua
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="#"
+                                class="text-gray-300 hover:text-white text-sm"
+                            >
+                                Ut enim ad minim veniam
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="w-full lg:w-3/12 px-4">
+                    <h3 class="font-bold text-xl mb-2 text-white">Contact</h3>
+                    <ul class="list-reset items-center">
+                        <li>
+                            <span class="text-gray-300 text-sm pr-4">
+                                Address:
+                            </span>
+                            <span class="text-gray-300 text-sm">
+                                1600 Amphitheatre Pkwy
+                            </span>
+                        </li>
+                        <li>
+                            <span class="text-gray-300 text-sm pr-4">
+                                Phone:
+                            </span>
+                            <a
+                                href="tel:+1-510-123-4567"
+                                class="text-gray-300 text-sm hover:text-white"
+                            >
+                                +1 510 123 4567
+                            </a>
+                        </li>
+                        <li>
+                            <span class="text-gray-300 text-sm pr-4">
+                                Email:
+                            </span>
+                            <a
+                                href="mailto:   "
+                                class="text-gray-300 text-sm hover:text-white"
+                            >
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent',
+    components: {},
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style></style>
