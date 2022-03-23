cd339a443c4c2f19acd3c2dfbcab260c1fb76557
rosaalwyh <alawiyahrosa@gmail.com>
2022-03-23 16:36:43 +0700

    login done

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
-
+let baseUrl = "http://localhost:3000"
 export default new Vuex.Store({
   state: {
+    users: {
+      username: "",
+      password: "",
+    },
+    isLogin: false
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    FETCH_STATUS_LOGIN(state, payload) {
+      state.isLogin = payload
+    },
   },
   actions: {
+    async loginHandler(context, payload) {
+      return await axios.post(`${baseUrl}/login`, {
+        username: payload.username,
+        password: payload.password
+      })
+    },
   },
-  modules: {
-  }
+  modules: {}
 })
