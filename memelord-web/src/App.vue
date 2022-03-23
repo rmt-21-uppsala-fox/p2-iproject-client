a0edb1945b7173348ad1095cd10707c33d98bc70
Rizki Arifudin <rizkiarifudin45@gmail.com>
2022-03-23 13:15:05 +0700

    init done, add navbar

---
+++ memelord-web/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <NavigationBar></NavigationBar>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavigationBar from "@/components/NavigationBar.vue";
+export default {
+  name: "app",
+  components: {
+    NavigationBar,
+  },
+};
+</script>
---
+++ memelord-web/src/components/NavigationBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav class="nav">
+      <div class="flex">
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "NavigationBar"
+};
+</script>
+
