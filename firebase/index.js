4a4bfded6e1865774f7338f91f7414b5ddad6d35
dzulfiqarzaky <dzulfiqarzakyaulia@gmail.com>
2022-03-23 16:07:43 +0700

    after check register to firebase

--- firebase/index.js
+++ firebase/index.js
@@ -1,36 +1,34 @@
 // import firebase
-import {} from 'dotenv/config';
+// import {} from 'dotenv/config';
 import { initializeApp } from 'firebase/app';
 import {
     getAuth,
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
 } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
-import { getFireStore } from 'firebase/firestore';
+// import { getDatabase } from 'firebase/database';
+// import { getFireStore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.WEBNOVEL_PROJECT_API_KEY,
-    authDomain: process.env.WEBNOVEL_PROJECT_AUTH_DOMAIN,
-    databaseURL: process.env.WEBNOVEL_PROJECT_DATABASE_URL,
-    projectId: process.env.WEBNOVEL_PROJECT_PROJECT_ID,
-    storageBucket: process.env.WEBNOVEL_PROJECT_STORAGE_BUCKET,
-    messagingSenderId: process.env.WEBNOVEL_PROJECT_MESSAGING_SENDER_ID,
-    appId: process.env.WEBNOVEL_PROJECT_APP_ID,
+    apiKey: 'AIzaSyDdA_OKvGulVAR-zjpU2RswYL51Op0fqR0',
+    authDomain: 'webnovel-project.firebaseapp.com',
+    projectId: 'webnovel-project',
+    storageBucket: 'webnovel-project.appspot.com',
+    messagingSenderId: '451164453364',
+    appId: '1:451164453364:web:5256b5a5354328358ca1a1',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
-const database = getDatabase(app);
-const db = getFireStore(app);
+// const database = getDatabase(app);
+// const db = getFireStore(app);
 
 export {
     auth,
-    database,
-    db,
+    // database,
+    // db,
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
-    sendPasswordResetEmail,
 };
