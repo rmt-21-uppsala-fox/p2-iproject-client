1f9932821a9ede8bdc4fb49a46eb95446bdb8452
Busyairi AR <busyairiar@gmail.com>
2022-03-23 17:34:10 +0700

    welcome form page & show all menu

--- natura/src/store/index.js
+++ natura/src/store/index.js
@@ -1,16 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '../api/axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    menus: [],
+    customerName: '',
+    phoneNumber: '',
+    totalPerson: '',
   },
   getters: {
   },
   mutations: {
+    FETCH_MENUS(state, payload) {
+      state.menus = payload;
+    },
+    SET_CUSTOMERNAME(state, payload) {
+      state.customerName = payload;
+    },
+    SET_PHONENUMBER(state, payload) {
+      state.phoneNumber = payload;
+    },
+    SET_TOTALPERSON(state, payload) {
+      state.totalPerson = payload;
+    }
   },
   actions: {
+    async fetchMenus (context) {
+      try {
+        const { data } = await axios.get('/menus') 
+        context.commit("FETCH_MENUS", data)
+      } catch (error) {
+        console.log(error.response.data)
+      }
+    }
   },
   modules: {
   }
---
+++ natura/src/views/MenuView.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-container class="h-100" >
+    <v-row>
+    <v-col :cols="2" class="sidebar-container">
+        <p>food</p>
+        <p>drink</p>
+    </v-col>
+      <v-col :cols="10" fluid class="menu-content">
+        <div fluid class="container-content" id="category.name" >
+        <h5 class="mb-3">Menu</h5>
+        <v-row dense>
+        <MenuCard/>
+          <MenuCard v-for="menu in menus" :key="menu.id" :menu="menu" />
+        </v-row>
+        </div>
+      </v-col >
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import MenuCard from '../components/MenuCard.vue'
+export default {
+  components: { MenuCard },
+    name: "MenuView",
+    methods: {
+        fetchMenus() {
+            this.$store.dispatch("fetchMenus")
+        }
+    },
+    computed: {
+        menus() {
+            return this.$store.state.menus;
+        }
+    },
+    created() {
+        this.fetchMenus()
+    }
+}
+</script>
+
+<style>
+.h-100 {
+  height: 100%;
+}
+
+.image-header {
+  height: 4vh;
+  width: 10vw;
+  margin: 0!important;
+}
+.sidebar-container {
+  position: sticky;
+  padding: 0!important;
+  height: 100vh !important;
+  max-height: 100vh! important;
+  overflow-y: scroll;
+}
+.menu-content {
+  padding: 10px !important;
+  height: 100vh !important;
+  max-height: 90vh! important;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+</style>
