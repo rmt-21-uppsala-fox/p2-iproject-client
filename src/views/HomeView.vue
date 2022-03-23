4da6e84227a3d34b5a46fcd7e9f9c3071d46e646
rosaalwyh <alawiyahrosa@gmail.com>
2022-03-23 17:05:15 +0700

    done fetch recipe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,12 +13,17 @@
             <div class="pb-10 text-center section-title">
               <div class="m-auto line"></div>
               <h3 class="title">Recommendation Recipes, <span> Based on today's weather!</span>
+                <p>Today's weather is {{recipes.weather}}, so we recommend you to makes this foods!</p>
               </h3>
             </div> <!-- section title -->
           </div>
         </div> <!-- row -->
         <div class="justify-center row">
-          <div class="w-full sm:w-2/3 lg:w-1/3">
+          <div
+            class="w-full sm:w-2/3 lg:w-1/3"
+            v-for="(recipe, index) in recipes.recipes"
+            :key="index"
+          >
             <div
               @click="gotoDetail"
               class="single-services wow fadeIn"
@@ -28,78 +33,12 @@
               <div class="services-icon">
                 <img
                   class="shape"
-                  src="assets/images/services-shape.svg"
+                  :src="recipe.recipe.image"
                   alt="shape"
                 >
-                <img
-                  class="shape-1"
-                  src="assets/images/services-shape-1.svg"
-                  alt="shape"
-                >
-                <i class="lni lni-baloon"></i>
               </div>
               <div class="mt-8 services-content">
-                <h4 class="mb-8 text-xl font-bold text-gray-900">Clean</h4>
-                <p class="mb-8">Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonu eirmod tempor
-                  invidunt labore.</p>
-                <a
-                  class="duration-300 hover:text-theme-color"
-                  href="javascript:void(0)"
-                >Learn More <i class="ml-2 lni lni-chevron-right"></i></a>
-              </div>
-            </div> <!-- single services -->
-          </div>
-          <div class="w-full sm:w-2/3 lg:w-1/3">
-            <div
-              class="mt-8 text-center single-services wow fadeIn"
-              data-wow-duration="1s"
-              data-wow-delay="0.5s"
-            >
-              <div class="services-icon">
-                <img
-                  class="shape"
-                  src="assets/images/services-shape.svg"
-                  alt="shape"
-                >
-                <img
-                  class="shape-1"
-                  src="assets/images/services-shape-2.svg"
-                  alt="shape"
-                >
-                <i class="lni lni-cog"></i>
-              </div>
-              <div class="mt-8 services-content">
-                <h4 class="mb-8 text-xl font-bold text-gray-900">Robust</h4>
-                <p class="mb-8">Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonu eirmod tempor
-                  invidunt labore.</p>
-                <a
-                  class="duration-300 hover:text-theme-color"
-                  href="javascript:void(0)"
-                >Learn More <i class="ml-2 lni lni-chevron-right"></i></a>
-              </div>
-            </div> <!-- single services -->
-          </div>
-          <div class="w-full sm:w-2/3 lg:w-1/3">
-            <div
-              class="mt-8 text-center single-services wow fadeIn"
-              data-wow-duration="1s"
-              data-wow-delay="0.8s"
-            >
-              <div class="services-icon">
-                <img
-                  class="shape"
-                  src="assets/images/services-shape.svg"
-                  alt="shape"
-                >
-                <img
-                  class="shape-1"
-                  src="assets/images/services-shape-3.svg"
-                  alt="shape"
-                >
-                <i class="lni lni-bolt-alt"></i>
-              </div>
-              <div class="mt-8 services-content">
-                <h4 class="mb-8 text-xl font-bold text-gray-900">Powerful</h4>
+                <h4 class="mb-8 text-xl font-bold text-gray-900">{{recipe.recipe.label}}</h4>
                 <p class="mb-8">Lorem ipsum dolor sit amet,consetetur sadipscing elitr, seddiam nonu eirmod tempor
                   invidunt labore.</p>
                 <a
@@ -134,10 +73,18 @@ export default {
     HeaderComponent,
     LogoApiComponent,
   },
+  computed: {
+    recipes() {
+      return this.$store.state.recipes;
+    },
+  },
   methods: {
     gotoDetail() {
       this.$router.push({ name: "detail-recipe" });
     },
+  },
+  created: function () {
+    this.$store.dispatch("fetchRecipes");
   },
 };
 </script>
