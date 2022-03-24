d4fa0b9b0e4faf124287b9db226b266558e29aa3
sammyon <samamandre.mandracon@gmail.com>
2022-03-24 11:56:36 +0700

    firebase

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,18 @@
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
+    <HFooter></HFooter>
   </div>
 </template>
+
+<script>
+import HFooter from 'vue-hacktiv8-footer'
+export default {
+  components: {
+    HFooter
+  }
+}
+</script>
 
 <style>
 /* #app {
