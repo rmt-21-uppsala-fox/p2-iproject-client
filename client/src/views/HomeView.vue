cb3c55b7af636a0d3d0206f46efdb98766e7be64
KevinWiranata97 <wiranatakevin89@gmail.com>
2022-03-25 01:01:48 +0700

    rapihkan search

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -9,11 +9,6 @@
           </div>
           <div class="search-box">
           <div>
-            <select name="" id="">
-              <option value="Everything">Everything</option>
-              <option value="Titles">Titles</option>
-              <option value="Descriptions">Descriptions</option>
-            </select>
             <input type="text" v-model="search" name="q" placeholder="search ..." />
             <button><i class="fa fa-search"></i></button>
           </div>
