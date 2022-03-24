7a9e59899d5f0ffd4fd0d73b7577a62a288ad3df
Rasyid619 <arifinrasyid13@gmail.com>
2022-03-24 07:39:38 +0700

    hacktive8 footer added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
 	<div id="app" class="bg-slate-900">
 		<router-view />
+		<HFooter></HFooter>
 	</div>
 </template>
+
+<script>
+	import HFooter from "vue-hacktiv8-footer";
+	export default {
+		components: {
+			HFooter,
+		},
+	};
+</script>
 
 <style></style>
