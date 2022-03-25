142e3371b6e4efcbeac7bcb5602e0a0217ef8817
arda <fatikaardha@gmail.com>
2022-03-25 19:48:10 +0700

    hotFix

--- src/apis/customerApi.js
+++ src/apis/customerApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const customerAPI = axios.create({
-  baseURL: "http://localhost:3000/",
-});
-
-// const production = axios.create({
-//   baseURL: "https://iproject-klikshop.herokuapp.com",
+// const customerAPI = axios.create({
+//   baseURL: "http://localhost:3000/",
 // });
 
-export default customerAPI;
+const production = axios.create({
+  baseURL: "https://iproject-klikshop.herokuapp.com",
+});
+
+export default production;
